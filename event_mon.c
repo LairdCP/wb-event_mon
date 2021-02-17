24ae19c036b1a14a57056e3e34ef92760ccf91f2
Service Build Slave <it.devops@lairdconnect.com>
2021-02-17 22:08:12 +0000

    bump to build number 7.3.0.26

--- event_mon.c
+++ event_mon.c
@@ -25,7 +25,7 @@ CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #include <string.h>
 #include "sdc_sdk.h"
 
-#define LRD_BLD_NUMBER  "7.3.0.25"
+#define LRD_BLD_NUMBER  "7.3.0.26"
 
 #define LRD_EVENT_MON_VERSION_MAJOR 3
 #define LRD_EVENT_MON_VERSION_MINOR 5
