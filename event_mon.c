5407d83a19b6952bf65cb0db8145639b2956dd18
Service Build Slave <it.devops@lairdconnect.com>
2022-07-14 02:27:17 +0000

    bump to build number 10.0.0.46

--- event_mon.c
+++ event_mon.c
@@ -25,7 +25,7 @@ CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #include <string.h>
 #include "sdc_sdk.h"
 
-#define LRD_BLD_NUMBER  "10.0.0.45"
+#define LRD_BLD_NUMBER  "10.0.0.46"
 
 #define LRD_EVENT_MON_VERSION_MAJOR 3
 #define LRD_EVENT_MON_VERSION_MINOR 5
